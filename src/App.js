8e9e8cd977165b939156624bc6c0eb63adafdfed
Artem Onufrei <2hilderoy2@gmail.com>
2020-10-12 09:51:33 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Word from './Word.js';
 
 function App() {
+
+  const [search, setSearch] = useState("");
+  const [words, setWords] = useState([]);
+  const [wordSize, setWordSize] = useState(1);
+
+  const searchFor = async (searchValue) => {
+    console.log(searchValue);
+    await fetch(`/find/${searchValue}`)
+    .then(response => response.json())
+    .then(data => setWords(data.words));  
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header-style">
+        <p className="header-title">Поле Чудес</p>
       </header>
+      <section className="search-section">
+        <div className="word-size-container">
+          <button className="arrow-container" onClick={() => { if(wordSize > 2 )  setWordSize(wordSize-1) } }><div className="left-arrow"></div></button>
+          <p className="word-size">{wordSize}</p>
+          <button className="arrow-container" onClick={() => { if(wordSize < 20)  setWordSize(wordSize+1) } }><div className="right-arrow"></div></button>
+        </div>
+        <div className="letter-container">
+          
+        </div>
+        <div>
+          <input type="text" placeholder="Поиск слова" value={search} onChange={e => setSearch(e.target.value)}/>
+          <button onClick={() => searchFor(search)}>Поиск</button>
+        </div>
+
+        <div className="words-container">
+          {words.map(word => (
+            <Word word={word.word} />
+          ))};  
+        </div>
+
+      </section>
+
+
+
     </div>
   );
 }
---
+++ src/Word.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+ 
+function Word({word}){
+    return(
+        <div>
+            <p>{word}</p>
+        </div>
+    );
+}
+
+export default Word;
