d6f3108bd349c14ecd4baab3fa0e975baad0c4af
Artem Onufrei <2hilderoy2@gmail.com>
2020-10-22 19:10:18 +0300

    Updates

---
+++ precache-manifest.d5c76c8ea70d6ad54538394fc4e6d8aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9464c72c3cce16b9c156e3d79fd2a2af",
+    "url": "/wonderfield-frontend/index.html"
+  },
+  {
+    "revision": "478ca6a07e5457838abb",
+    "url": "/wonderfield-frontend/static/css/main.98923738.chunk.css"
+  },
+  {
+    "revision": "04a9b700cdadee231945",
+    "url": "/wonderfield-frontend/static/js/2.eb11405a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wonderfield-frontend/static/js/2.eb11405a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478ca6a07e5457838abb",
+    "url": "/wonderfield-frontend/static/js/main.2066803d.chunk.js"
+  },
+  {
+    "revision": "93a81afc7563e9ad8816",
+    "url": "/wonderfield-frontend/static/js/runtime-main.1a039d24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wonderfield-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
